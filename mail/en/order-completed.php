61afd86efb3384d56aa75bf5c72c54efcb424e72
Pierre M <pierre@Mac-de-Pierre.local>
2015-02-02 15:20:06 +0100

    updated print/send mail

---
+++ mail/en/order-completed.php
@@ -0,0 +1,23 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this \yii\web\View view component instance */
+/* @var $message \yii\mail\MessageInterface the message being composed */
+/* @var $content string main view render result */
+?>
+
+Your order <?=$model->name ?> is ready.
+
+--
+Labo JJ Micheli
+
+Open Mondays to Fridays from 8h30 to 18h30.
+
+21-23 rue de Tervaete, 1040 Brussels
+Phone: +32 (0)2 733 21 85
+e-Mail: info@labojjmicheli.be
+www.labojjmicheli.be
+
+BNP Paribas Fortis Bank
+IBAN: BE55 2100 3814 9344 - BIC: GEBABEBB
+VAT: BE 428 746 631
