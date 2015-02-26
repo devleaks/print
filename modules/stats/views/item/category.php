a29cbce5c37b545faa7609adba6366675bddb0bf
Pierre M <pierre@Mac-de-Pierre.local>
2015-02-26 16:26:58 +0100

    Added stats

---
+++ modules/stats/views/item/category.php
@@ -0,0 +1,72 @@
+<?php
+
+use app\models\Item;
+use miloschuman\highcharts\Highcharts;
+use yii\grid\GridView;
+use yii\helpers\Html;
+use yii\helpers\VarDumper;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\ParameterSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+$data = [];
+foreach($dataProvider->query->each() as $m) {
+	$data[] = [$m['category'] == ''? 'Sans' :$m['category'], intval($m['tot_count'])];
+}
+
+$this->title = Yii::t('store', 'Items');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('store', 'Statistics'), 'url' => ['/stats']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="parameter-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?php echo '';/* GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'item_id',
+            'total',
+        ],
+    ]); 	<?= VarDumper::dump($data) ?> */ ?>
+
+
+
+
+	<?= Highcharts::widget([
+		'options' => [
+		    'title' => ['text' => Yii::t('store', 'Items')],
+			'tooltip' => [
+				'pointFormat' => '{series.name}: <b>{point.percentage:.1f}%</b>'
+			],
+			'series' => [ [
+				'type' => 'pie',
+				'name' => 'Item',
+				'data' => $data
+				]
+			]
+		]
+	]);?>
+
+</div>
+<script type="text/javascript">
+<?php $this->beginBlock('JS_RADIALIZE_COLORS'); ?>
+// Radialize the colors
+Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function (color) {
+    return {
+        radialGradient: { cx: 0.5, cy: 0.3, r: 0.7 },
+        stops: [
+            [0, color],
+            [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+        ]
+    };
+});
+<?php $this->endBlock(); ?>
+</script>
+<?php
+$this->registerJs($this->blocks['JS_RADIALIZE_COLORS'], yii\web\View::POS_END);
+
+
+
