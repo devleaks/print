3a9924c8ae9af189eb77abe6884f17c9e99aec39
Pierre M <pierre@Mac-de-Pierre.local>
2015-03-24 15:19:48 +0100

    BoM cannot change client

--- modules/order/controllers/DocumentController.php
+++ modules/order/controllers/DocumentController.php
@@ -167,8 +167,13 @@ class DocumentController extends Controller
      */
     public function actionChangeClient($id) {
 		$model = $this->findModel($id);
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            ; //@setflash?
+
+		
+		if($model->bom_bool) {
+			Yii::$app->session->setFlash('danger', Yii::t('store', 'You cannot change the client of a Bill of Material or of a bill of BoM.'));
+			return $this->redirect(Yii::$app->request->referrer);
+		} else if ($model->load(Yii::$app->request->post()) && $model->save()) {
+			Yii::$app->session->setFlash('success', Yii::t('store', '{document} updated', ['document' => Yii::t('store', $model->document_type)]).'.');
         }
         return $this->render('change-client', [
             'model' => $model,
