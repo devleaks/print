bb2af9e32f5c07188270575011bb11bcc6bead91
Pierre M <pierre@Mac-de-Pierre.local>
2015-05-26 10:50:11 +0200

    dezip in /tmp

--- runtime/etc/restore.sh
+++ runtime/etc/restore.sh
@@ -6,10 +6,10 @@ cd $dir
 
 if [ -f $YIIDIR/runtime/restore/$DBNAME.gz -a -f $YIIDIR/runtime/restore/media.taz ]
 then
-  gunzip < $YIIDIR/runtime/restore/$DBNAME.gz > $DBNAME.sql
+  gunzip < $YIIDIR/runtime/restore/$DBNAME.gz > /tmp/$DBNAME.sql
   mysql -u$DBNAME -p$DBNAME $DBNAME < $YIIDIR/runtime/etc/drop_all_tables.sql
-  mysql -u$DBNAME -p$DBNAME $DBNAME < $DBNAME.sql
-  echo rm $DBNAME.sql
+  mysql -u$DBNAME -p$DBNAME $DBNAME < /tmp/$DBNAME.sql
+  echo rm /tmp/$DBNAME.sql
   ( cd $YIIDIR/web ; rm -rf pictures documents ; tar xzf $YIIDIR/runtime/restore/media.taz )
   # sudo -u daemon chown -R daemon:daemon $YIIDIR/web/documents $YIIDIR/web/pictures $YIIDIR/runtime
   echo "Restored."
