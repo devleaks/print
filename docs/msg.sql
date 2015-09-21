9dbf847d9f19a9c4ce4eb8b893b03c95214a47a1
Pierre M <devleaks.be@gmail.com>
2015-09-21 10:01:10 +0200

    temporaily pushed messages

---
+++ docs/msg.sql
@@ -0,0 +1,5 @@
+INSERT INTO `parameter` (`domain`, `name`, `lang`, `value_text`, `value_number`, `value_int`, `value_date`, `created_at`, `updated_at`)
+VALUES
+	('communication', 'signature', 'en', 'Please check your order, dimensions and options. Please mention your order number on all communications and when paying by bank transfer. Thank You.', NULL, NULL, NULL, NULL, NULL),
+	('communication', 'signature', 'fr', 'Veuillez, s\'il vous plaît, vérifier votre commande, les dimensions, et les options de finition. Veuillez également rappeler le numéro du bon de commande lors de', NULL, NULL, NULL, NULL, NULL),
+	('communication', 'signature', 'nl', 'Kunt u, alstublieft, controle meting en afwerkingsopties. Vermeld uw ordernummer in alle communicatie en betalingen. Dank U.', NULL, NULL, NULL, NULL, NULL);
