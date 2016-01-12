9a49d3c9d9a2ec43a446318a282e8d54b7315100
Pierre M <devleaks.be@gmail.com>
2016-01-12 17:45:51 +0100

    version printing in extract is now optional (parameter)

--- modules/accnt/views/extraction/_extract.php
+++ modules/accnt/views/extraction/_extract.php
@@ -1,15 +1,17 @@
 <?php
+use app\models\Parameter;
+
 $year = null;
 if($model = $models->one()) {
 	$year = substr($model->name, 2, 2);
 } else {
 	$year = date('y');
 }
-$ver = `git describe --tags`;
-
+$ver = Parameter::isTrue('popsy', 'extract_version') ? ' Rel. '.`git describe --tags` : '
+';
 ?>
-|     Popsy file Rel. <?= $ver ?>
-
+|     Popsy file<?= $ver
+?>
 CreateKeyCustomer:Y
 IgnoreAnalClosed:Y
 DossierSelect:001
