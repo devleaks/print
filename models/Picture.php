be5bfab159cca9d45e0d524b7320d10b7f44bbe2
Pierre M <devleaks.be@gmail.com>
2016-02-16 14:10:13 +0100

    suppressed error when image does not exist on copy

--- models/Picture.php
+++ models/Picture.php
@@ -113,16 +113,18 @@ class Picture extends _Picture
 	 * @param string dst Destination folder path
 	 */
 	private function recurse_copy($src,$dst) {
-	    $dir = opendir($src);
-	    @mkdir($dst);
-	    while(false !== ($file = readdir($dir)) )
-	        if (($file != '.') && ($file != '..')) {
-	            if (is_dir($src . DIRECTORY_SEPARATOR . $file))
-	                recurse_copy($src . DIRECTORY_SEPARATOR . $file,$dst . DIRECTORY_SEPARATOR . $file);
-	            else
-	                copy($src . DIRECTORY_SEPARATOR . $file,$dst . DIRECTORY_SEPARATOR . $file);
-	        }
-	    closedir($dir);
+	    if(is_dir($src)) {		
+			$dir = opendir($src);
+		    @mkdir($dst);
+		    while(false !== ($file = readdir($dir)) )
+		        if (($file != '.') && ($file != '..')) {
+		            if (is_dir($src . DIRECTORY_SEPARATOR . $file))
+		                recurse_copy($src . DIRECTORY_SEPARATOR . $file,$dst . DIRECTORY_SEPARATOR . $file);
+		            else
+		                copy($src . DIRECTORY_SEPARATOR . $file,$dst . DIRECTORY_SEPARATOR . $file);
+		        }
+		    closedir($dir);
+		}
 	}
 
 	/**
